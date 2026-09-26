e4c5b4ebbc2a6a7806dfe977660fc874258e6219
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:45:30 +0300

    chore(content): simplify empty legacy page module

--- src/content/pages/discourse-production-2.ts
+++ src/content/pages/discourse-production-2.ts
@@ -1,9 +1,3 @@
 import type { GrammarPage } from "../types";
-import { ex, mistake, page } from "../helpers";
 
-const G = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
-const C = "https://ciberduvidas.iscte-iul.pt/consultorio/areas/gramatica/3";
-const CAM = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-
-export const pages: GrammarPage[] = [
-  ];
+export const pages: GrammarPage[] = [];
