40092e53d5408b2975d07569638bce30f0488016
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:45:19 +0300

    chore(content): simplify empty legacy page module

--- src/content/pages/choice-and-determiners-production.ts
+++ src/content/pages/choice-and-determiners-production.ts
@@ -1,10 +1,3 @@
 import type { GrammarPage } from "../types";
-import { ex, mistake, page } from "../helpers";
 
-const G = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
-const G3 = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-iii/";
-const C = "https://ciberduvidas.iscte-iul.pt/gramatica";
-const CAM = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-
-export const pages: GrammarPage[] = [
-  ];
+export const pages: GrammarPage[] = [];
