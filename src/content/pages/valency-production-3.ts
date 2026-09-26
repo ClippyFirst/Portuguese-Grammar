ca82567881c594bccbd4c9de13b4d3f74be1cbd8
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:46:15 +0300

    chore(content): simplify empty legacy page module

--- src/content/pages/valency-production-3.ts
+++ src/content/pages/valency-production-3.ts
@@ -1,9 +1,3 @@
 import type { GrammarPage } from "../types";
-import { ex, mistake, page } from "../helpers";
 
-const GULBENKIAN_II = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
-const CIBERDUVIDAS_REG = "https://ciberduvidas.iscte-iul.pt/consultorio/areas/regencia/9";
-const CAMOES_PLE = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-
-export const pages: GrammarPage[] = [
-  ];
+export const pages: GrammarPage[] = [];
