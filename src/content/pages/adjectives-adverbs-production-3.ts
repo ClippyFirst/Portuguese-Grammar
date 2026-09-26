943ba291795df58c786db93870a01c052a7934d5
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:45:11 +0300

    chore(content): simplify empty legacy page module

--- src/content/pages/adjectives-adverbs-production-3.ts
+++ src/content/pages/adjectives-adverbs-production-3.ts
@@ -1,9 +1,3 @@
 import type { GrammarPage } from "../types";
-import { ex, mistake, page } from "../helpers";
 
-const G1 = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-i/";
-const CAM = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-const CIB = "https://ciberduvidas.iscte-iul.pt/";
-
-export const pages: GrammarPage[] = [
-  ];
+export const pages: GrammarPage[] = [];
