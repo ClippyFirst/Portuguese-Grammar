f5e4a2b084fcf5e02361b5ad1530e86af523b644
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:47:31 +0300

    chore(qa): remove remaining lint and workflow warnings

--- src/content/pages/coverage-closure-production-2.ts
+++ src/content/pages/coverage-closure-production-2.ts
@@ -3,7 +3,6 @@ import { ex, mistake, page } from "../helpers";
 
 const G2 = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
 const C = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-const CD = "https://ciberduvidas.iscte-iul.pt/";
 
 export const pages: GrammarPage[] = [
   page({
