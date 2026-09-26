e2cf280602beaba4826c7c6aa1413c35578e080c
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:46:20 +0300

    chore(content): simplify empty legacy page module

--- src/content/pages/valency-syntax-closure-production.ts
+++ src/content/pages/valency-syntax-closure-production.ts
@@ -1,9 +1,3 @@
 import type { GrammarPage } from "../types";
-import { ex, mistake, page } from "../helpers";
 
-const G2 = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
-const C = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
-const CD = "https://ciberduvidas.iscte-iul.pt/";
-
-export const pages: GrammarPage[] = [
-  ];
+export const pages: GrammarPage[] = [];
