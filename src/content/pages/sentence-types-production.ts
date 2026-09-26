e67c518d8384dffa28619a9b24c3c393e73c7359
The Clippy First <zemlekop17@gmail.com>
2026-09-26 19:47:36 +0300

    chore(qa): remove remaining lint and workflow warnings

--- src/content/pages/sentence-types-production.ts
+++ src/content/pages/sentence-types-production.ts
@@ -1,7 +1,6 @@
 import type { GrammarPage } from "../types";
 import { ex, mistake, page } from "../helpers";
 
-const GULBENKIAN_II = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-ii/";
 const GULBENKIAN_III = "https://gulbenkian.pt/publications/gramatica-do-portugues-vol-iii/";
 const CAMOES_PLE = "https://www.instituto-camoes.pt/activity/centro-virtual/referencial-camoes-ple";
 
